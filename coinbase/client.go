ce3fc3c0f432c44bdaa97b9f110f068b52cadb1f
Jesus Diaz <jesus@tryminded.com>
2022-02-16 16:16:08 -0300

    implementing the Subscribe func

--- coinbase/client.go
+++ coinbase/client.go
@@ -1,9 +1,12 @@
 package coinbase
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 
 	ws "golang.org/x/net/websocket"
+	"golang.org/x/xerrors"
 )
 
 type client struct {
@@ -22,4 +25,68 @@ func NewClient(endpoint string) (CoinbaseWsClient, error) {
 	return &client{
 		conn: conn,
 	}, nil
+}
+
+func (c *client) Subscribe(ctx context.Context, tradingPairs []string, receiver chan Response) error {
+	if len(tradingPairs) == 0 {
+		return xerrors.New("tradingPairs must have a least one pair")
+	}
+
+	subscription := Request{
+		Type:       RequestTypeSubscribe,
+		ProductIDs: tradingPairs,
+		Channels: []Channel{
+			{Name: ChannelTypeMatches},
+		},
+	}
+
+	payload, err := json.Marshal(subscription)
+	if err != nil {
+		return xerrors.Errorf("failed to marshal subscription: %w", err)
+	}
+
+	err = ws.Message.Send(c.conn, payload)
+	if err != nil {
+		return xerrors.Errorf("failed to send subscription: %w", err)
+	}
+
+	var subscriptionResponse Response
+
+	err = ws.JSON.Receive(c.conn, &subscriptionResponse)
+	if err != nil {
+		return xerrors.Errorf("failed to receive subscription response: %w", err)
+	}
+
+	if RequestType(subscriptionResponse.Type) == RequestTypeError {
+		return xerrors.Errorf("failed to subscribe: %s", subscriptionResponse.Message)
+	}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				err := c.conn.Close()
+				if err != nil {
+					log.Printf("failed closing ws connection: %s", err)
+				}
+			default:
+				var message Response
+
+				err := ws.JSON.Receive(c.conn, &message)
+				if err != nil {
+					log.Printf("failed receiving message: %s", err)
+					break
+				}
+
+				receiver <- Response{
+					Type:      message.Type,
+					Size:      message.Size,
+					Price:     message.Price,
+					ProductID: message.ProductID,
+				}
+			}
+		}
+	}()
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module vwap-calculator
 
 go 1.17
 
-require golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+require (
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+)
